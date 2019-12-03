46caa372171cc2edcfa5dd244a07c0319cd16fe8
James-14 <ls198506@gmail.com>
2019-12-03 20:18:33 +0800

    完成多入口和多htmlWebpackPlugin插件的自动写入

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
 const path = require('path');
+const glob = require("glob");
 
 const extractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+
+//读取所有.js文件,动态设置多入口
+function getEntry() {
+	var entry = {};
+	//读取src目录下page下的所有.js文件
+	glob.sync('./src/pages/**/*.js')
+		.forEach(function (name) {
+            let start = name.indexOf('src/') + 10,
+				end = name.length - 3;
+            let n = name.slice(start, end);
+            let key = n.slice(0, n.lastIndexOf('/')); //保存各个组件的入口
+            // console.log(key);
+			entry[key] = name;
+		});
+	return entry;
+};
 
 module.exports = {
     //配置多个入口
-    entry: {
-        'index': './src/pages/index/index.js',
-        'home': './src/pages/home/index.js',
-    },
+    // entry: {
+    //     'index': './src/pages/index/index.js',
+    //     'home': './src/pages/home/index.js',
+    // },
+    entry:getEntry(),
     output: {
         //__dirname node.js的一个全局环境变量,用于指向当前执行脚本（dirname.js）所在的目录路径，而且是绝对路径
         path: path.resolve(__dirname, 'dist'),
@@ -51,21 +70,64 @@ module.exports = {
             // filename: 'css/[name].[hash:8].min.css',
             filename: path.posix.join('assets', '/css/[name].[hash:8].min.css'),
         }),
-        new HtmlWebpackPlugin({
-            title: '多页面开发框架',
-            template: './src/pages/index/index.html', // 源模板文件
-            filename: './index/index.html', // 输出文件【注意：这里的根路径是module.exports.output.path】
-            showErrors: true,
-            inject: 'true',          //所有JavaScript资源插入到body元素的底部
-            chunks: ["common", 'index'] //配置写入哪些js和css
-        }),
-        new HtmlWebpackPlugin({
-            title: '多页面开发框架',
-            template: './src/pages/home/index.html', // 源模板文件
-            filename: './home/index.html', // 输出文件【注意：这里的根路径是module.exports.output.path】
-            showErrors: true,
-            inject: 'true',          //所有JavaScript资源插入到body元素的底部
-            chunks: ["common", 'home'] //配置写入哪些js和css
-        }),
+        // new HtmlWebpackPlugin({
+        //     title: '多页面开发框架',
+        //     template: './src/pages/index/index.html', // 源模板文件
+        //     filename: './index/index.html', // 输出文件【注意：这里的根路径是module.exports.output.path】
+        //     showErrors: true,
+        //     inject: 'true',          //所有JavaScript资源插入到body元素的底部
+        //     chunks: ["common", 'index'] //配置写入哪些js和css
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: '多页面开发框架',
+        //     template: './src/pages/home/index.html', // 源模板文件
+        //     filename: './home/index.html', // 输出文件【注意：这里的根路径是module.exports.output.path】
+        //     showErrors: true,
+        //     inject: 'true',          //所有JavaScript资源插入到body元素的底部
+        //     chunks: ["common", 'home'] //配置写入哪些js和css
+        // }),
     ],
 }
+
+// 设置html-webpack-plugin参数，返回参数对象
+let getHtmlConfig = function (name, chunks) {
+	var _template = `./src/pages/${name}/index.html`;
+	var _filename = `${name}/index.html`;
+	//index单独处理
+	if (name === "index") {
+		_filename = `index.html`;
+	}
+	let config = {
+		template: _template,
+		filename: _filename,
+		// favicon: './favicon.ico',
+		// title: title,
+		inject: true, //设置为true插入的元素放入body元素的底部
+		hash: true, //开启hash  ?[hash]
+		chunks: chunks,
+		minify: process.env.NODE_ENV === "development" ? false : {
+			removeComments: true, //移除HTML中的注释
+			collapseWhitespace: true, //折叠空白区域 也就是压缩代码
+			removeAttributeQuotes: true, //去除属性引用
+		}
+	};
+	return config;
+};
+
+//插入htmlWebpackPlugin
+ (function(){
+    //取得配置的入口key
+    const entryObj = getEntry();
+    //存储路径和chunks
+    const htmlArray = [];
+    for(let key in entryObj){
+        htmlArray.push({
+            html: key,
+            chunks: ['vendor', 'common', key]
+        })
+    }
+    //自动生成html模板
+    htmlArray.forEach((element) => {
+        module.exports.plugins.push(new htmlWebpackPlugin(getHtmlConfig(element.html, element.chunks)));
+    });
+ })();
